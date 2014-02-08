fa68675229ff4e4bd02655aa276ad512e658b3fb
Carson Gee <x@carsongee.com>
2014-02-08 16:34:59 -0500

    Update __init__.py

--- xsiftx/sifters/__init__.py
+++ xsiftx/sifters/__init__.py
@@ -6,7 +6,7 @@ The expectations of sifters are that the first line output
 is the filename to use, and everything else on stdout is
 the file to upload to the dashboard.
 
-You can write to stderr without consequence in neccessary,
+You can write to stderr without consequence if neccessary,
 and returning anything but 0 will cause the upload to be
 aborted. Command is run with the following arguments:
 <sifter> edx_venv_path edx_platform_path course_id [extra_arg, extra_arg,....]
